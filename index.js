aa50ac6316d83ad61810ad0174bf1c2d401c2e01
vmwhite <30372917+vmwhite@users.noreply.github.com>
2021-08-28 20:44:08 -0500

    pushing branch changes

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","4vh 4vw");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
+        if (document.body.classList.contains("dark-mode")) {
+            $("#header").css("background","rgb(13, 0, 3)");
+            $("#header").css("color","#fff");
+            $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+            $("#header").css("padding","4vh 4vw");
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        } else {
+            $("#header").css("background","#fff");
+            $("#header").css("color","#000");
+            $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+            $("#header").css("padding","4vh 4vw");
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        }
     }else{
         $("#header").css("background","transparent");
         $("#header").css("color","#fff");
@@ -65,7 +77,60 @@ $(document).ready(function(){
       });
   });
   
-  function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+  function themeToggle() {
+    var body = document.body
+    var newClass = body.className == 'dark-mode' ? 'light-mode' : 'dark-mode'
+    body.className = newClass
+    var endDate = new Date();
+    endDate.setFullYear(endDate.getFullYear() + 10);
+        
+    if ((width >= 1000)){
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            if (document.body.classList.contains("dark-mode")) {
+                $("#header").css("background","rgb(13, 0, 3)");
+                $("#header").css("color","#fff");
+                $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+                $("#header").css("padding","4vh 4vw");
+                $("#navigation a").hover(function(){
+                    $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+                },function(){
+                    $(this).css("border-bottom","2px solid transparent");
+                });
+            } else {
+                $("#header").css("background","#fff");
+                $("#header").css("color","#000");
+                $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+                $("#header").css("padding","4vh 4vw");
+                // $("#navigation a").css("color","#fff"); attempt to make header labels same at the rest
+                $("#navigation a").hover(function(){
+                    $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+                },function(){
+                    $(this).css("border-bottom","2px solid transparent");
+                });
+            }
+        }else{
+            $("#header").css("background","transparent");
+            $("#header").css("color","#fff");
+            $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+            $("#header").css("padding","6vh 4vw");
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid #fff");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        }
+    }
+
+    document.cookie = 'theme=' + (newClass == 'light-mode' ? 'light' : 'dark') +
+    '; Expires=' + endDate + ';'
+    console.log('Cookies are now: ' + document.cookie)
   }
+
+  function isDarkThemeSelected() {
+    return document.cookie.match(/theme=dark/i) != null
+  }
+
+  function setThemeFromCookie() {
+    document.body.className = isDarkThemeSelected() ? 'dark-mode' : 'light-mode'
+  }
+
